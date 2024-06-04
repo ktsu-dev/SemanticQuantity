9e7737482d241329b33734a14798c7beaec7fde5
matt-edmondson <matthew.edmondson@gmail.com>
2024-06-04 23:03:52 +1000

    Remove unnescessary record constructors

--- SemanticQuantity/SemanticQuantity.cs
+++ SemanticQuantity/SemanticQuantity.cs
@@ -2,77 +2,87 @@ namespace ktsu.io.SemanticQuantity;
 
 using System.Numerics;
 
-public abstract record SemanticQuantity<TStorage>(TStorage Quantity)
+public abstract record SemanticQuantity<TStorage>
 	where TStorage : INumber<TStorage>
 {
+	public TStorage Quantity { get; protected set; } = TStorage.Zero;
+
+	public static TQuantity Create<TQuantity>(TStorage quantity)
+		where TQuantity : SemanticQuantity<TStorage>, new()
+		=> new TQuantity() with { Quantity = quantity };
 }
 
-public abstract record SemanticQuantity<TSelf, TStorage>(TStorage Quantity)
-	: SemanticQuantity<TStorage>(Quantity)
+public abstract record SemanticQuantity<TSelf, TStorage>
+	: SemanticQuantity<TStorage>
 	, IAdditionOperators<SemanticQuantity<TSelf, TStorage>, TSelf, TSelf>
 	, ISubtractionOperators<SemanticQuantity<TSelf, TStorage>, TSelf, TSelf>
 	, IMultiplyOperators<SemanticQuantity<TSelf, TStorage>, TStorage, TSelf>
 	, IDivisionOperators<SemanticQuantity<TSelf, TStorage>, TStorage, TSelf>
 	, IDivisionOperators<SemanticQuantity<TSelf, TStorage>, SemanticQuantity<TSelf, TStorage>, TStorage>
 	, IUnaryNegationOperators<SemanticQuantity<TSelf, TStorage>, TSelf>
-	where TSelf : SemanticQuantity<TSelf, TStorage>
+	where TSelf : SemanticQuantity<TSelf, TStorage>, new()
 	where TStorage : INumber<TStorage>
 {
-	public static TQuantity Create<TQuantity>(TStorage quantity)
-	{
-		var outType = typeof(TQuantity);
-		var inType = typeof(TStorage);
-		return (TQuantity)(outType == inType
-			? quantity
-			: Activator.CreateInstance(outType, quantity)!);
-	}
-
 	public static TResult Multiply<TResult>(SemanticQuantity<TSelf, TStorage> self, SemanticQuantity<TStorage> other)
+		where TResult : SemanticQuantity<TStorage>, new()
 	{
 		ArgumentNullException.ThrowIfNull(self);
 		ArgumentNullException.ThrowIfNull(other);
-		return Create<TResult>(self.Quantity * other.Quantity);
+		return Create<TResult>(self.Quantity * other.Quantity)!;
 	}
 
 	public static TResult Multiply<TResult>(SemanticQuantity<TSelf, TStorage> self, TStorage other)
+		where TResult : SemanticQuantity<TStorage>, new()
 	{
 		ArgumentNullException.ThrowIfNull(self);
 		ArgumentNullException.ThrowIfNull(other);
-		return Create<TResult>(self.Quantity * other);
+		return Create<TResult>(self.Quantity * other)!;
 	}
 
 	public static TResult Divide<TResult>(SemanticQuantity<TSelf, TStorage> self, SemanticQuantity<TStorage> other)
+		where TResult : SemanticQuantity<TStorage>, new()
 	{
 		ArgumentNullException.ThrowIfNull(self);
 		ArgumentNullException.ThrowIfNull(other);
-		return Create<TResult>(self.Quantity / other.Quantity);
+		return Create<TResult>(self.Quantity / other.Quantity)!;
 	}
 
 	public static TResult Divide<TResult>(SemanticQuantity<TSelf, TStorage> self, TStorage other)
+		where TResult : SemanticQuantity<TStorage>, new()
 	{
 		ArgumentNullException.ThrowIfNull(self);
 		ArgumentNullException.ThrowIfNull(other);
-		return Create<TResult>(self.Quantity / other);
+		return Create<TResult>(self.Quantity / other)!;
+	}
+
+	public static TStorage DivideToStorage(SemanticQuantity<TSelf, TStorage> self, SemanticQuantity<TStorage> other)
+	{
+		ArgumentNullException.ThrowIfNull(self);
+		ArgumentNullException.ThrowIfNull(other);
+		return self.Quantity / other.Quantity;
 	}
 
 	public static TResult Add<TResult>(SemanticQuantity<TSelf, TStorage> self, SemanticQuantity<TStorage> other)
+		where TResult : SemanticQuantity<TStorage>, new()
 	{
 		ArgumentNullException.ThrowIfNull(self);
 		ArgumentNullException.ThrowIfNull(other);
-		return Create<TResult>(self.Quantity + other.Quantity);
+		return Create<TResult>(self.Quantity + other.Quantity)!;
 	}
 
 	public static TResult Subtract<TResult>(SemanticQuantity<TSelf, TStorage> self, SemanticQuantity<TStorage> other)
+		where TResult : SemanticQuantity<TStorage>, new()
 	{
 		ArgumentNullException.ThrowIfNull(self);
 		ArgumentNullException.ThrowIfNull(other);
-		return Create<TResult>(self.Quantity - other.Quantity);
+		return Create<TResult>(self.Quantity - other.Quantity)!;
 	}
 
 	public static TResult Negate<TResult>(SemanticQuantity<TSelf, TStorage> self)
+		where TResult : SemanticQuantity<TStorage>, new()
 	{
 		ArgumentNullException.ThrowIfNull(self);
-		return Create<TResult>(-self.Quantity);
+		return Create<TResult>(-self.Quantity)!;
 	}
 
 	public static TSelf operator +(SemanticQuantity<TSelf, TStorage> left, TSelf right) => Add<TSelf>(left, right);
@@ -80,5 +90,5 @@ public abstract record SemanticQuantity<TSelf, TStorage>(TStorage Quantity)
 	public static TSelf operator -(SemanticQuantity<TSelf, TStorage> value) => Negate<TSelf>(value);
 	public static TSelf operator *(SemanticQuantity<TSelf, TStorage> left, TStorage right) => Multiply<TSelf>(left, right);
 	public static TSelf operator /(SemanticQuantity<TSelf, TStorage> left, TStorage right) => Divide<TSelf>(left, right);
-	public static TStorage operator /(SemanticQuantity<TSelf, TStorage> left, SemanticQuantity<TSelf, TStorage> right) => Divide<TStorage>(left, (TSelf)right);
+	public static TStorage operator /(SemanticQuantity<TSelf, TStorage> left, SemanticQuantity<TSelf, TStorage> right) => DivideToStorage(left, right);
 }
--- Test/Tests.cs
+++ Test/Tests.cs
@@ -6,15 +6,16 @@ using ktsu.io.SemanticQuantity;
 [TestClass]
 public class Tests
 {
-	public record LinearDistance<TStorage>(TStorage BaseQuantity)
-		: SemanticQuantity<LinearDistance<TStorage>, TStorage>(BaseQuantity)
+	public record LinearDistance<TStorage>
+		: SemanticQuantity<LinearDistance<TStorage>, TStorage>
 		, IMultiplyOperators<LinearDistance<TStorage>, LinearDistance<TStorage>, Area<TStorage>>
 		where TStorage : INumber<TStorage>
 	{
 		public static Area<TStorage> operator *(LinearDistance<TStorage> left, LinearDistance<TStorage> right) => Multiply<Area<TStorage>>(left, right);
 	}
 
-	public record Area<TStorage>(TStorage BaseQuantity) : SemanticQuantity<Area<TStorage>, TStorage>(BaseQuantity)
+	public record Area<TStorage>
+		: SemanticQuantity<Area<TStorage>, TStorage>
 		, IMultiplyOperators<Area<TStorage>, LinearDistance<TStorage>, Volume<TStorage>>
 		, IDivisionOperators<Area<TStorage>, LinearDistance<TStorage>, LinearDistance<TStorage>>
 		where TStorage : INumber<TStorage>
@@ -23,7 +24,8 @@ public class Tests
 		public static LinearDistance<TStorage> operator /(Area<TStorage> left, LinearDistance<TStorage> right) => Divide<LinearDistance<TStorage>>(left, right);
 	}
 
-	public record Volume<TStorage>(TStorage BaseQuantity) : SemanticQuantity<Volume<TStorage>, TStorage>(BaseQuantity)
+	public record Volume<TStorage>
+		: SemanticQuantity<Volume<TStorage>, TStorage>
 		, IDivisionOperators<Volume<TStorage>, LinearDistance<TStorage>, Area<TStorage>>
 		, IDivisionOperators<Volume<TStorage>, Area<TStorage>, LinearDistance<TStorage>>
 		where TStorage : INumber<TStorage>
@@ -35,74 +37,74 @@ public class Tests
 	[TestMethod]
 	public void TestMultiplication()
 	{
-		var distance = new LinearDistance<int>(5);
-		var area = new Area<int>(10);
-		var volume = new Volume<int>(15);
+		var distance = SemanticQuantity<int>.Create<LinearDistance<int>>(5);
+		var area = SemanticQuantity<int>.Create<Area<int>>(10);
+		var volume = SemanticQuantity<int>.Create<Volume<int>>(15);
 		var calculatedDistance = distance * 5;
 		var calculatedArea = area * 5;
 		var calculatedVolume = volume * 5;
 		var derivedArea = distance * distance;
 		var derivedVolume = area * distance;
-		Assert.AreEqual(25, calculatedDistance.BaseQuantity);
-		Assert.AreEqual(50, calculatedArea.BaseQuantity);
-		Assert.AreEqual(75, calculatedVolume.BaseQuantity);
-		Assert.AreEqual(25, derivedArea.BaseQuantity);
-		Assert.AreEqual(50, derivedVolume.BaseQuantity);
+		Assert.AreEqual(25, calculatedDistance.Quantity);
+		Assert.AreEqual(50, calculatedArea.Quantity);
+		Assert.AreEqual(75, calculatedVolume.Quantity);
+		Assert.AreEqual(25, derivedArea.Quantity);
+		Assert.AreEqual(50, derivedVolume.Quantity);
 	}
 
 	[TestMethod]
 	public void TestDivision()
 	{
-		var distance = new LinearDistance<int>(5);
-		var area = new Area<int>(10);
-		var volume = new Volume<int>(15);
+		var distance = SemanticQuantity<int>.Create<LinearDistance<int>>(5);
+		var area = SemanticQuantity<int>.Create<Area<int>>(10);
+		var volume = SemanticQuantity<int>.Create<Volume<int>>(15);
 		var calculatedDistance = distance / 5;
 		var calculatedArea = area / 5;
 		var calculatedVolume = volume / 5;
 		var derivedArea = volume / distance;
 		var derivedDistance = volume / area;
 		int derivedRatio = volume / volume;
-		Assert.AreEqual(1, calculatedDistance.BaseQuantity);
-		Assert.AreEqual(2, calculatedArea.BaseQuantity);
-		Assert.AreEqual(3, calculatedVolume.BaseQuantity);
-		Assert.AreEqual(3, derivedArea.BaseQuantity);
-		Assert.AreEqual(1, derivedDistance.BaseQuantity);
+		Assert.AreEqual(1, calculatedDistance.Quantity);
+		Assert.AreEqual(2, calculatedArea.Quantity);
+		Assert.AreEqual(3, calculatedVolume.Quantity);
+		Assert.AreEqual(3, derivedArea.Quantity);
+		Assert.AreEqual(1, derivedDistance.Quantity);
 		Assert.AreEqual(1, derivedRatio);
 	}
 
 	[TestMethod]
 	public void TestAddition()
 	{
-		var distance = new LinearDistance<int>(5);
-		var area = new Area<int>(10);
-		var volume = new Volume<int>(15);
+		var distance = SemanticQuantity<int>.Create<LinearDistance<int>>(5);
+		var area = SemanticQuantity<int>.Create<Area<int>>(10);
+		var volume = SemanticQuantity<int>.Create<Volume<int>>(15);
 		var calculatedDistance = distance + distance;
 		var calculatedArea = area + area;
 		var calculatedVolume = volume + volume;
-		Assert.AreEqual(10, calculatedDistance.BaseQuantity);
-		Assert.AreEqual(20, calculatedArea.BaseQuantity);
-		Assert.AreEqual(30, calculatedVolume.BaseQuantity);
+		Assert.AreEqual(10, calculatedDistance.Quantity);
+		Assert.AreEqual(20, calculatedArea.Quantity);
+		Assert.AreEqual(30, calculatedVolume.Quantity);
 	}
 
 	[TestMethod]
 	public void TestSubtraction()
 	{
-		var distance = new LinearDistance<int>(5);
-		var area = new Area<int>(10);
-		var volume = new Volume<int>(15);
+		var distance = SemanticQuantity<int>.Create<LinearDistance<int>>(5);
+		var area = SemanticQuantity<int>.Create<Area<int>>(10);
+		var volume = SemanticQuantity<int>.Create<Volume<int>>(15);
 		var calculatedDistance = distance - distance;
 		var calculatedArea = area - area;
 		var calculatedVolume = volume - volume;
-		Assert.AreEqual(0, calculatedDistance.BaseQuantity);
-		Assert.AreEqual(0, calculatedArea.BaseQuantity);
-		Assert.AreEqual(0, calculatedVolume.BaseQuantity);
+		Assert.AreEqual(0, calculatedDistance.Quantity);
+		Assert.AreEqual(0, calculatedArea.Quantity);
+		Assert.AreEqual(0, calculatedVolume.Quantity);
 	}
 
 	[TestMethod]
 	public void TestNegation()
 	{
-		var distance = new LinearDistance<int>(5);
+		var distance = SemanticQuantity<int>.Create<LinearDistance<int>>(5);
 		var negatedDistance = -distance;
-		Assert.AreEqual(-5, negatedDistance.BaseQuantity);
+		Assert.AreEqual(-5, negatedDistance.Quantity);
 	}
 }
